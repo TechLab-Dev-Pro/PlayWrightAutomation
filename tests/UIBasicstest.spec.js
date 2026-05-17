eae0db441afc6a9c7e7871ac545ceb6e3a9297ce
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-17 16:45:50 +0200

    Tests Commit

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,4 +1,4 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 
 test('Browser Context Playwright test', async ({browser})=>
@@ -7,13 +7,18 @@ test('Browser Context Playwright test', async ({browser})=>
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://www.youtube.com/watch?v=ncZ8SnPHYYA");
+    console.log(await page.title());
+
 
 
 });
 
 test('Page Playwright test', async ({page})=>
 {
-    await page.goto("https://www.youtube.com/watch?v=ncZ8SnPHYYA");
+    await page.goto("https://google.com");
+    //get titrle - assertion
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
     //
 
 
