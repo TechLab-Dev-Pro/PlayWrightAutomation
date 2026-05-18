d0272e71c9bfe12143a786949c80f3fc863265e0
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-18 23:08:14 +0200

    2

--- playwright.config.js
+++ playwright.config.js
@@ -7,9 +7,9 @@ import { defineConfig, devices } from '@playwright/test';
 const config = ({
   testDir: './tests',
   // Only if I want to override the 30s timeout I write this bellow
-  timeout: 40 *1000,
+  timeout: 30 *1000,
    expect: {
-    timeout: 40 *1000,
+    timeout: 5000,
    },
    reporter : 'html',
 
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,13 +1,19 @@
 const {test, expect} = require('@playwright/test');
 
 
-test('Browser Context Playwright test', async ({browser})=>
+test.only('Browser Context Playwright test', async ({browser})=>
 {
     //chrome - plugins/ cookies
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://www.youtube.com/watch?v=ncZ8SnPHYYA");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
+    //css   type, filll
+    await page.locator('#username').fill("rahulshetty");
+    await page.locator("[type='password']").fill("learning");
+    await page.locator("#signInBtn").click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 
 
 
