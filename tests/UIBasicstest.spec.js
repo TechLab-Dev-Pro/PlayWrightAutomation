d1e281cd76724a13c59e73fc63da829eabc0943c
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-20 22:32:48 +0200

    4

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,13 +1,14 @@
 const {test, expect} = require('@playwright/test');
 
 
-test.only('Browser Context Playwright test', async ({browser})=>
+test.only('Browser Context-Validating Error login', async ({browser})=>
 {
     //chrome - plugins/ cookies
     const context = await browser.newContext();
     const page = await context.newPage();
     const userName = page.locator('#username');
     const signIn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
     //css   type, filll
@@ -20,8 +21,10 @@ test.only('Browser Context Playwright test', async ({browser})=>
     await userName.fill("");
     await userName.fill("rahulshettyacademy");
     await signIn.click();
-    console.log(await page.locator(".card-body a").first().textContent());
-   console.log(await page.locator(".card-body a").nth(1).textContent());
+    console.log(await cardTitles.first().textContent());
+   console.log(await cardTitles.nth(1).textContent());
+   const allTitles = await cardTitles.allTextContents();
+   console.log(allTitles);
 
 
 
