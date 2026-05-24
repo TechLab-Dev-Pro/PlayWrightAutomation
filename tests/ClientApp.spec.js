d99b3411df8bee000fdeecebb248cfd76f369acc
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-24 13:29:14 +0200

    9

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,16 +1,39 @@
 const {test, expect} = require('@playwright/test');
 
 
-test('@WC Client App login', async ({page})=> {
 
+
+test('@WC Client App login', async ({page})=> 
+{
+    const productName = 'Zara Coat 4';
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("anshika@gmail.com");
     await page.locator("#userPassword").type("Iamking@000");
     await page.locator("[value='Login']").click();
-    // await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle');
     await page.locator(".card-body b").first().waitFor();
     const titles= await page.locator(".card-body b").allTextContents();
     console.log(titles);
+    const count = await products.count();
+    for(let i = 0; i < count; ++i)
+    {
+    if(await products.nth(i).locator("b").textContent() === productName)
+    {
+        //add to cart
+        await products.nth(i).locator("text= Add To Cart").click();
+        break;
+    }
+    }
+    await page.locator("[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+
+    const bool = await page.locator("h5:has-text('ZARA COAT 3").isVisible();
+    expect(bool).toBeTruthy();
+
+
+    //Zara Coat 4
+
 
 
 
