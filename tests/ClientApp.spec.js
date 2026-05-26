73da9b0971a001e70f3f50f0c2421071a262f6ff
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-26 21:21:11 +0200

    11

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -45,7 +45,26 @@ test('@WC Client App login', async ({page})=>
                 break;
             }
     }
-    await page.pause()
+        expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+        await page.locator(".action__submit").click();
+        expect(page.locator("hero-primary")).toHaveText(" Thank you for the order.");
+        const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+        console.log(orderId);
+         
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+ 
+ 
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
     //Zara Coat 4
 
