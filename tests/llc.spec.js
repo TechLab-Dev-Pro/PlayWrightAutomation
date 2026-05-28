f5436dd3281d9e358ad8a49525ae3c520d3dfea5
TechLab-Dev-Pro <258237329+TechLab-Dev-Pro@users.noreply.github.com>
2026-05-28 23:13:57 +0200

    13

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -6,6 +6,11 @@ test("Playwright Special locators", async ({page}) => {
     await page.getByLabel("Check me out of you Love Icecream!").click();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    await page.getByRole("button", {name: 'Submit'}).click();
+    await page.getByText("Success! The Form has been submitted successfully!").isVisible();
+    await page.getByRole("link", {name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText: "Nokia Edge"}).getByRole("button").click();
 
 
 });
